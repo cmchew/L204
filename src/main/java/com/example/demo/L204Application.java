5a6dcb9bf13ff52e9f90be19159d4bfa9a2f9899
cmchew <write_on@hotmail.com>
2020-01-08 11:13:52 -0500

    L204

---
+++ src/main/java/com/example/demo/L204Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L204Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L204Application.class, args);
+    }
+
+}
